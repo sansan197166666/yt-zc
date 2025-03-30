19edbbfaeb9080249c42a0fc7d78fa756cc0192a
kuaifengwozhanghao <lebronjames2938311@outlook.com>
2025-03-30 23:29:36 +0800

    FloatingWindowService.kt

--- flutter/android/app/src/main/kotlin/com/carriez/flutter_hbb/FloatingWindowService.kt
+++ flutter/android/app/src/main/kotlin/com/carriez/flutter_hbb/FloatingWindowService.kt
@@ -28,6 +28,7 @@ import android.widget.PopupMenu
 import com.caverock.androidsvg.SVG
 import ffi.FFI
 import kotlin.math.abs
+import android.content.Context
 
 class FloatingWindowService : Service(), View.OnTouchListener {
 
