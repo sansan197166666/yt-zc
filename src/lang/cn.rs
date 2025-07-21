905b3fd70e3f7173c8698355a37df4c187606696
sansan197166666 <sansan197166666@gmail.com>
2025-07-21 21:20:08 +0800

    cn.rs

--- src/lang/cn.rs
+++ src/lang/cn.rs
@@ -172,7 +172,7 @@ pub static ref T: std::collections::HashMap<&'static str, &'static str> =
         ("Local Port", "本地端口"),
         ("Local Address", "当前地址"),
         ("Change Local Port", "修改本地端口"),
-        ("setup_server_tip", "联系客服"),
+        ("setup_server_tip", " "),
         ("Too short, at least 6 characters.", "太短了，至少 6 个字符"),
         ("The confirmation is not identical.", "两次输入不匹配"),
         ("Permissions", "权限"),
